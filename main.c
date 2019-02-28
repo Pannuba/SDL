59b27488f8891901a6d0448ae1ee7295595cde8f
Pannuba <carlomaria.99@hotmail.it>
2019-03-01 00:06:28 +0100

    vsync, inizio timer

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include "init.h"
 #include "foto.h"
 
+#define SCREEN_FPS 60
+#define SCREEN_TICKS_PER_FRAME  (1000 / SCREEN_FPS)
 #define TOTAL_KEYS 2
 
 typedef enum {false, true} bool;
@@ -14,6 +16,8 @@ int main()
 	Foto foto;
 	initialize(&foto);
 	bool quit = false;
+	int seconds = 0, countedFrames = 0;
+	float avgFPS = 0;
 	
 	if (init(&window, &screenSurface) == false)
 	{
@@ -23,7 +27,7 @@ int main()
 	
 	else
 	{
-		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+		renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
 		if (loadMedia(image) == false)
 		{
@@ -39,12 +43,26 @@ int main()
 					quit = true;				
 			}
 
+			countedFrames++;
+			seconds = SDL_GetTicks() / 1000;
+			printf("%d\n", seconds);
+			avgFPS = countedFrames / (float)seconds;	//creare file timer!
+			printf("FPS: %f\n", avgFPS);
+
+			if (avgFPS > 200000000)
+				avgFPS = 0;
+
 			handleEvent(&foto, &currentSurface);
-			
+
 			SDL_RenderClear(renderer);
 			render(&foto, renderer);
 
 			SDL_RenderPresent(renderer);
+
+			/*countedFrames++;printf("ticks %d\n", SDL_GetTicks());
+int ticks = SDL_GetTicks();
+			if (ticks < SCREEN_TICKS_PER_FRAME)
+				SDL_Delay(SCREEN_TICKS_PER_FRAME - ticks);*/
 		}
 	}
 
