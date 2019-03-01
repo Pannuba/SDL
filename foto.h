9a22c88a3b3b3ac748060b3552d832551d6dbdf3
Giulio Occhipinti <occhipinti99@disroot.org>
2019-03-01 23:54:43 +0100

    fix leak, oop

--- foto.h
+++ foto.h
@@ -1,22 +1,7 @@
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 #ifndef FOTO_H
 #define FOTO_H
+#include "object.h"
 
-typedef struct foto
-{
-	SDL_Surface *loadedSurface;
-	SDL_Texture *texture;
-	int width;
-	int height;
-	int posX;
-	int posY;
-} Foto;
-
-void initialize(Foto *foto);
-
-void handleEvent(Foto *foto, SDL_Surface **currentSurface);
-
-void render(Foto *foto, SDL_Renderer *renderer);
+void handleEvent(Object *foto);
 
 #endif
